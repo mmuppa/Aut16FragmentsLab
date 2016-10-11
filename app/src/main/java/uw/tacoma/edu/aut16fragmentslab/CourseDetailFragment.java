aa6aa57a73c5476b661b6a0763e65f76f6b69bbb
mmuppa <mmuppa@uw.edu>
2016-10-10 18:48:18 -0700

    Initial commit

---
+++ app/src/main/java/uw/tacoma/edu/aut16fragmentslab/CourseDetailFragment.java
@@ -0,0 +1,111 @@
+package uw.tacoma.edu.aut16fragmentslab;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import uw.tacoma.edu.aut16fragmentslab.course.CourseContent;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CourseDetailFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * create an instance of this fragment.
+ */
+public class CourseDetailFragment extends Fragment {
+
+    public static String DETAIL_ARG = "detail_arg";
+    private OnFragmentInteractionListener mListener;
+
+    public CourseDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method
+        // below that sets the article text.
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            updateCourseItemView((CourseContent.CourseItem) args.getSerializable(DETAIL_ARG));
+        } else {
+            // Set article based on saved instance state defined during onCreateView
+            updateCourseItemView(CourseContent.ITEMS.get(0));
+        }
+    }
+
+    public void updateCourseItemView(CourseContent.CourseItem item) {
+        TextView courseIdTextView = (TextView) getActivity().findViewById(R.id.course_item_id);
+        courseIdTextView.setText(item.id);
+        TextView courseTitleTextView = (TextView) getActivity().findViewById(R.id.course_item_title);
+        courseTitleTextView.setText(item.title);
+        TextView courseShortDescTextView = (TextView) getActivity().findViewById(R.id.course_item_desc);
+        courseShortDescTextView.setText(item.shortDescription);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course_detail, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
